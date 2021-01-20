cd8e7c3e6e9c3ded5f9ef77d21eaf4ad7e4c0d96
SvjetlanaC <svjetlana.cvijanovic@student.etf.unibl.org>
2021-01-21 00:25:05 +0100

    2.Provjera postojanja PIN-a (u aplikaciji za evidentiranje vremena)

--- main.c
+++ main.c
@@ -3,45 +3,66 @@
 #include <time.h>
 #include <string.h>
 
-void aplikacijaZaEvidentiranjeVremena()
+void aplikacijaZaEvidentiranjeVremena(int pin)
 {
-    int pin;
-    printf("Unesite PIN kod:");
-    scanf("%d",&pin);
     FILE *dat;
     char osoba[25];
-    char s[6];
+    char s[3];
     itoa(pin,osoba,10);
     strcat(osoba,".txt");
     if((dat=fopen(osoba,"a")))
     {
         time_t t = time(0);
         struct tm tm = *localtime(&t);
-        while(1)
+        printf("\nOpcije:\n");
+        printf(" -Dolazak na posao [D]\n -Odlazak sa posla [O]\n");
+        printf("\nODABERITE OPCIJU:");
+        scanf("%s",s);
+        if(strcmp(s,"D")==0)
         {
-            printf("Dolazak na posao [D], odlazak sa posla [O] ili nazad[N]:");
-            scanf("%s",s);
-            if(strcmp(s,"D")==0)
-            {
-                fprintf(dat,"Vrijeme dolaska na posao: %02d-%02d-%d %02d:%02d:%02d\n",tm.tm_mday, tm.tm_mon + 1,tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec);
-                printf("Trenutno vrijeme uspjesno upisano u registar za radno vrijeme!");
-                break;
-            }
-            else if(strcmp(s,"O")==0)
-            {
-                fprintf(dat,"Vrijeme odlaska sa posla: %02d-%02d-%d %02d:%02d:%02d\n",tm.tm_mday, tm.tm_mon + 1,tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec);
-                printf("Trenutno vrijeme uspjesno upisano u registar za radno vrijeme!");
-                break;
-            }
-            else if(strcmp(s,"N")==0)
-                break;
+            fprintf(dat,"Vrijeme dolaska na posao: %02d-%02d-%d %02d:%02d:%02d\n",tm.tm_mday, tm.tm_mon + 1,tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec);
+            printf("\nTrenutno vrijeme uspjesno upisano u registar za radno vrijeme!\n");
+        }
+        else if(strcmp(s,"O")==0)
+        {
+            fprintf(dat,"Vrijeme odlaska sa posla: %02d-%02d-%d %02d:%02d:%02d\n",tm.tm_mday, tm.tm_mon + 1,tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec);
+            printf("\nTrenutno vrijeme uspjesno upisano u registar za radno vrijeme!\n");
         }
     }
-    else printf("Greška pri otvaranju datoteke!");
+    else printf("Greška pri otvaranju datoteke!\n");
+    fclose(dat);
 }
 int main()
 {
-    aplikacijaZaEvidentiranjeVremena();
-
+    int pin;
+    int pinDat;
+    int flag=0;
+    int n=0;
+    FILE *file;
+    while(n<3)
+    {
+        printf("Unesite PIN kod:");
+        scanf("%d",&pin);
+        if((file=fopen("korisnickiNalozi.txt","r")))
+        {
+            while((fscanf(file,"%*d %*s %*s %d",&pinDat))!=EOF)
+            {
+                if(pin==pinDat)
+                {
+                    flag=1;
+                    aplikacijaZaEvidentiranjeVremena(pin);
+                }
+            }
+            if(flag==0)
+            {
+                n++;
+                printf("Uneseni PIN kod ne postoji! Imate jos %d %s\n\n",(3-n),((3-n)>1)? "pokusaja!" : "pokusaj!" );
+            }
+            else break;
+        }
+        else
+            printf("Greska pri otvaranju datoteke!\n");
+    }
+    fclose(file);
     return 0;
 }
